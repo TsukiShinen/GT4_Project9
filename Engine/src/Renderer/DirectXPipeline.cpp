82d099d18602638815678a175577188af0fdad82
TsukiShinen <nino.guyon@gmail.com>
2023-12-26 11:26:35 +0100

    feat: Use Constant Buffer

--- Engine/src/Renderer/DirectXPipeline.cpp
+++ Engine/src/Renderer/DirectXPipeline.cpp
@@ -28,10 +28,10 @@ namespace Engine
 
         CD3DX12_DESCRIPTOR_RANGE cbvTable0;
         cbvTable0.Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, 1, 0);
-        slotRootParameter[0].InitAsDescriptorTable(1, &cbvTable0);
+        slotRootParameter[0].InitAsConstantBufferView(0);
         CD3DX12_DESCRIPTOR_RANGE cbvTable1;
         cbvTable1.Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, 1, 1);
-        slotRootParameter[1].InitAsDescriptorTable(1, &cbvTable1);
+        slotRootParameter[1].InitAsConstantBufferView(1);
 
         const CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(2, slotRootParameter, 0, nullptr,
                                                       D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
--- Engine/src/Renderer/Model.cpp
+++ Engine/src/Renderer/Model.cpp
@@ -10,31 +10,14 @@ namespace Engine
     Model::Model(std::vector<Vertex>& pVertices, std::vector<uint16_t>& pIndices)
         : m_IndexCount(pIndices.size())
     {
-        // ===== Constant Buffer Heaps =====
-        D3D12_DESCRIPTOR_HEAP_DESC cbvHeapDesc;
-        cbvHeapDesc.NumDescriptors = 1;
-        cbvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
-        cbvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
-        cbvHeapDesc.NodeMask = 0;
-        ThrowIfFailed(DirectXContext::Get()->m_Device->CreateDescriptorHeap(&cbvHeapDesc,
-            IID_PPV_ARGS(&m_ConstantBufferHeap)));
-
+        // ===== Constant Buffer =====
         m_ConstantBuffer = std::make_unique<UploadBuffer<ObjectConstants>>(DirectXContext::Get()->m_Device.Get(), 1, true);
-
-        const auto handle = CD3DX12_CPU_DESCRIPTOR_HANDLE(m_ConstantBufferHeap->GetCPUDescriptorHandleForHeapStart());
-        const UINT objCbByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
-
-        D3D12_CONSTANT_BUFFER_VIEW_DESC cbvDesc;
-        cbvDesc.BufferLocation = m_ConstantBuffer->Resource()->GetGPUVirtualAddress();
-        cbvDesc.SizeInBytes = objCbByteSize;
-
-        DirectXContext::Get()->m_Device->CreateConstantBufferView(&cbvDesc, handle);
 
         // ===== Data =====
         DirectXContext::Get()->m_CommandObject->ResetList(DirectXContext::Get()->m_CommandObject->GetCommandAllocator());
 
-        auto verticesByteSize = static_cast<UINT>(pVertices.size()) * sizeof(Vertex);
-        auto indicesByteSize = static_cast<UINT>(pVertices.size()) * sizeof(Vertex);
+        const auto verticesByteSize = static_cast<UINT>(pVertices.size()) * sizeof(Vertex);
+        const auto indicesByteSize = static_cast<UINT>(pVertices.size()) * sizeof(Vertex);
         
         ThrowIfFailed(D3DCreateBlob(verticesByteSize, &m_Geometry.VertexBufferCPU));
         CopyMemory(m_Geometry.VertexBufferCPU->GetBufferPointer(), pVertices.data(), verticesByteSize);
@@ -74,9 +57,6 @@ namespace Engine
             m_NumFramesDirty--;
         }
 
-        ID3D12DescriptorHeap* descriptorHeaps[] = { m_ConstantBufferHeap.Get()};
-        DirectXContext::Get()->m_CommandObject->GetCommandList()->SetDescriptorHeaps(_countof(descriptorHeaps), descriptorHeaps);
-
         DirectXContext::Get()->m_CommandObject->GetCommandList()->SetPipelineState(DirectXContext::Get()->m_BasePipeline->GetState().Get());
         DirectXContext::Get()->m_CommandObject->GetCommandList()->SetGraphicsRootSignature(DirectXContext::Get()->m_BasePipeline->GetSignature().Get());
 
@@ -87,7 +67,7 @@ namespace Engine
         DirectXContext::Get()->m_CommandObject->GetCommandList()->IASetPrimitiveTopology(m_PrimitiveType);
 
         // Offset to the CBV in the descriptor heap for this object and for this frame resource.
-        DirectXContext::Get()->m_CommandObject->GetCommandList()->SetGraphicsRootDescriptorTable(0, m_ConstantBufferHeap->GetGPUDescriptorHandleForHeapStart());
+        DirectXContext::Get()->m_CommandObject->GetCommandList()->SetGraphicsRootConstantBufferView(0, m_ConstantBuffer->Resource()->GetGPUVirtualAddress());
         
         DirectXContext::Get()->m_CommandObject->GetCommandList()->DrawIndexedInstanced(m_IndexCount, 1, 0, 0, 0);
     }
